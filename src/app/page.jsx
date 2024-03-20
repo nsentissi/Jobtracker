689a948410b4ede94621166c608f458daf350825
Nawfal Sentissi <sentissinawfal@gmail.com>
2024-03-20 11:19:38 +0100

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="p-3">
+      <div>
+          
+      </div>
+      <div>
+
+      </div>
+    </main>
+  );
+}
