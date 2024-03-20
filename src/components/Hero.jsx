0c57e1497c62fd1122970f162376d7448d7b747e
Nawfal Sentissi <sentissinawfal@gmail.com>
2024-03-20 12:08:51 +0100

    created general layout for project and navbar

---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='w-full flex justify-center mt-24'>
+      <section className=''>
+        <h3>Navigate Your Career Path with Ease</h3>
+        <div></div>
+      </section>
+      <section></section>
+    </div>
+  )
+}
+
+export default Hero
